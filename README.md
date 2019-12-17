61a5b0a4d03a4c8a4e0bc6004d92a3e453ea8526
苍海之南 <1782920040@qq.com>
2019-12-17 12:18:35 +0800

    Fix：为feign引入http连接池

---
+++ README.md
@@ -0,0 +1,47 @@
+爱分享1024小程序后端
+
+这是我学习慕课的Spring Cloud Alibaba从入门到进阶课程代码。目前此小程序已部署在服务器，此代码clone之后需配合前端代码使用，nacos配置如下： 
+
+content-center-dev.yml 
+```xml
+mini:
+  appKey: 申请的appkey
+  appSecret: 申请的appSecret
+
+spring:
+  datasource:
+    url: jdbc:mysql://localhost/love-sharing?useSSL=false&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true&serverTimezone=GMT%2B8&nullCatalogMeansCurrent=true
+    username: root
+    password: root
+  application:
+    name: user-center
+  cloud:
+    nacos:
+      discovery:
+        server-addr: localhost:8848
+      config:
+        server-addr: localhost:8848
+  jackson:
+    date-format: yyyy-MM-dd
+    time-zone: GMT+8
+management:
+  endpoints:
+    web:
+      exposure:
+        include: '*'
+  endpoint:
+    health:
+      show-details: always
+feign:
+  hystrix:
+    enabled: true
+  httpclient:
+    # 让feign使用apache httpclient做请求；而不是默认的urlconnection
+    enabled: true
+    # feign的最大连接数
+    max-connections: 200
+    # feign单个路径的最大连接数
+    max-connections-per-route: 50
+```
+user-center-dev.yml基本同上，把application name改了就行`user-center`
+
---
+++ sql/love-sharing.sql
@@ -0,0 +1,94 @@
+﻿SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bonus_event_log
+-- ----------------------------
+DROP TABLE IF EXISTS `bonus_event_log`;
+CREATE TABLE `bonus_event_log`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id',
+  `user_id` int(11) DEFAULT NULL COMMENT 'user.id',
+  `value` int(11) DEFAULT NULL COMMENT '积分操作值',
+  `event` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '发生的事件',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_bonus_event_log_user1_idx`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '积分变更记录表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for mid_user_share
+-- ----------------------------
+DROP TABLE IF EXISTS `mid_user_share`;
+CREATE TABLE `mid_user_share`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `share_id` int(11) NOT NULL COMMENT 'share.id',
+  `user_id` int(11) NOT NULL COMMENT 'user.id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_mid_user_share_share1_idx`(`share_id`) USING BTREE,
+  INDEX `fk_mid_user_share_user1_idx`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户-分享中间表【描述用户购买的分享】' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for notice
+-- ----------------------------
+DROP TABLE IF EXISTS `notice`;
+CREATE TABLE `notice`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '内容',
+  `show_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否显示 0:否 1:是',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for rocketmq_transaction_log
+-- ----------------------------
+DROP TABLE IF EXISTS `rocketmq_transaction_log`;
+CREATE TABLE `rocketmq_transaction_log`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `transaction_Id` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '事务id',
+  `log` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '日志',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'RocketMQ事务日志表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for share
+-- ----------------------------
+DROP TABLE IF EXISTS `share`;
+CREATE TABLE `share`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(11) NOT NULL DEFAULT 0 COMMENT '发布人id',
+  `title` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '标题',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  `is_original` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否原创 0:否 1:是',
+  `author` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '作者',
+  `cover` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '封面',
+  `summary` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '概要信息',
+  `price` int(11) NOT NULL DEFAULT 0 COMMENT '价格（需要的积分）',
+  `download_url` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '下载地址',
+  `buy_count` int(11) NOT NULL DEFAULT 0 COMMENT '下载数 ',
+  `show_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否显示 0:否 1:是',
+  `audit_status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '审核状态 NOT_YET: 待审核 PASSED:审核通过 REJECTED:审核不通过',
+  `reason` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '审核不通过原因',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分享表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id',
+  `wx_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信id',
+  `wx_nickname` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信昵称',
+  `roles` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色',
+  `avatar_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '头像地址',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  `bonus` int(11) NOT NULL DEFAULT 300 COMMENT '积分',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分享' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
